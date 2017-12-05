71a23f16db37ef0810b5cb793e5bab8ca1d07ab7
Antonio <antoniogamiz10@gmail.com>
2017-12-05 19:10:47 +0100

    añadido GAMEOVER

--- text.py
+++ text.py
@@ -3,6 +3,7 @@ import pygame
 import random
 from pygame.locals import *
 
+# Dimensiones de la pantalla de juego.
 SCREEN_WIDTH=600
 SCREEN_HEIGHT=900
 
@@ -59,14 +60,18 @@ class Colliders(object):
             pygame.draw.rect(surface, RED, rec)
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, image):
+    def __init__(self, image, explosion_image):
         self.image=image
+        self.explosion_image=explosion_image
         self.rect=self.image.get_rect()
         self.rect.top=SCREEN_HEIGHT-200
         self.rect.left=SCREEN_WIDTH/2 - 50
         self.image=pygame.transform.scale(self.image, (100,100))
+        self.explosion_image=pygame.transform.scale(self.explosion_image, (200,200))
     def move(self, vx, vy):
         self.rect.move_ip(vx,vy)
+    def kill(self):
+        self.image = self.explosion_image
     def update(self, surface):
         surface.blit(self.image, self.rect)
 
@@ -74,7 +79,6 @@ class Player(pygame.sprite.Sprite):
 
 
 def main():
-    # Dimensiones de la pantalla de juego.
 
     pygame.init()
     screen = pygame.display.set_mode([SCREEN_WIDTH,SCREEN_HEIGHT])
@@ -87,8 +91,9 @@ def main():
     key_pressed = [False, False, False, False]
 
     # Creación del player.
+    explosion=load_image(".\\images\\explosion.png", True)
     ship=load_image(".\\images\\ship.png", True)
-    player=Player(ship)
+    player=Player(ship, explosion)
 
     # Creación del fondo.
     bg=load_image(".\\images\\background.png").convert_alpha()
@@ -98,46 +103,51 @@ def main():
     coll=Colliders(25)
     main_clock = pygame.time.Clock()
 
+
+    GAMEOVER = False
     exit= False
     
     while not exit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
-
-        #Gestión del movimiento de 'player'.
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT:
-                key_pressed[0] = True
-                vx=-speed
-            if event.key == pygame.K_RIGHT:
-                key_pressed[1] = True
-                vx=speed
-            if event.key == pygame.K_UP:
-                key_pressed[2] = True
-                vy=-speed
-            if event.key == pygame.K_DOWN:
-                key_pressed[3] = True
-                vy=speed
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_LEFT:
-                key_pressed[0] = False
-                if key_pressed[1]: vx=speed
-                else: vx=0
-            if event.key == pygame.K_RIGHT:
-                key_pressed[1] = False
-                if key_pressed[0]: vx=-speed
-                else: vx=0
-            if event.key == pygame.K_UP:
-                key_pressed[2] = False
-                if key_pressed[3]: vy=speed
-                else: vy=0
-            if event.key == pygame.K_DOWN:
-                key_pressed[3] = False
-                if key_pressed[2]: vy=-speed
-                else: vy=0
-        player.move(vx,vy)
-        coll.move()
+        if not GAMEOVER:
+            #Gestión del movimiento de 'player'.
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    key_pressed[0] = True
+                    vx=-speed
+                if event.key == pygame.K_RIGHT:
+                    key_pressed[1] = True
+                    vx=speed
+                if event.key == pygame.K_UP:
+                    key_pressed[2] = True
+                    vy=-speed
+                if event.key == pygame.K_DOWN:
+                    key_pressed[3] = True
+                    vy=speed
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_LEFT:
+                    key_pressed[0] = False
+                    if key_pressed[1]: vx=speed
+                    else: vx=0
+                if event.key == pygame.K_RIGHT:
+                    key_pressed[1] = False
+                    if key_pressed[0]: vx=-speed
+                    else: vx=0
+                if event.key == pygame.K_UP:
+                    key_pressed[2] = False
+                    if key_pressed[3]: vy=speed
+                    else: vy=0
+                if event.key == pygame.K_DOWN:
+                    key_pressed[3] = False
+                    if key_pressed[2]: vy=-speed
+                    else: vy=0
+            player.move(vx,vy)
+            coll.move()
+            if collision_detect(player, coll):
+                GAMEOVER=True
+                player.kill()
 
 
         main_clock.tick(1500)
