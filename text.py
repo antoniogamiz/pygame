87d1e1afdf0bb83ea301be1bb982a3a704db3387
Antonio <antoniogamiz10@gmail.com>
2017-12-06 13:18:57 +0100

    add timer

--- text.py
+++ text.py
@@ -13,6 +13,12 @@ BLACK=(0,0,0)
 RED=(255,0,0)
 GREEN=(0,255,0)
 
+# Tamaño de la fuente.
+FONT_SIZE = 40
+
+# Volumen de los sonidos.
+SOUND_VOLUME=0.0
+
 # Función auxiliar para cargar imágenes (con transparencia si se quiere)
 def load_image(filename, transparent=False):
     image = pygame.image.load(filename)
@@ -45,11 +51,11 @@ class Colliders(object):
         self.list=[]
         for x in range(init_n):
             # Creamos un rectángulo aleatorio.
-            self.list.append(pygame.Rect(random_rect_coord_generate(2, SCREEN_WIDTH, -400, -20, 10, 30, 15, 35)))
+            self.list.append(pygame.Rect(random_rect_coord_generate(2, SCREEN_WIDTH, -400, -20, 10, 15, 10, 15)))
     def re_add(self):
         for x in range(len(self.list)):
             if self.list[x].top > SCREEN_HEIGHT:
-                self.list[x]=pygame.Rect(random_rect_coord_generate(2, SCREEN_WIDTH, -400, -20, 10, 30, 15, 35))
+                self.list[x]=pygame.Rect(random_rect_coord_generate(2, SCREEN_WIDTH, -400, -20, 10, 15, 10, 15))
     def add_other(self):
         pass
     def move(self):
@@ -64,10 +70,10 @@ class Player(pygame.sprite.Sprite):
         self.movement_sound=movement_sound
         self.image=image.subsurface(270,120,400,550)
         self.explosion_image=explosion_image.subsurface(70,115,90,60)
+        self.image=pygame.transform.scale(self.image, (50,50))
         self.rect=self.image.get_rect()
         self.rect.top=SCREEN_HEIGHT-200
         self.rect.left=SCREEN_WIDTH/2 - 50
-        self.image=pygame.transform.scale(self.image, (50,50))
         self.explosion_image=pygame.transform.scale(self.explosion_image, (50,50))
     def move(self, vx, vy):
         self.rect.move_ip(vx,vy)
@@ -90,6 +96,7 @@ def main():
     # Cargamos la música de fondo.
     pygame.mixer.music.load(".\\music\\background_music.mp3")
     movement_sound=pygame.mixer.Sound(".\\music\\movement_sound.wav")
+    movement_sound.set_volume(SOUND_VOLUME)
 
     # Variables auxiliares para el control del movimiento.
     vx,vy = 0,0
@@ -107,22 +114,27 @@ def main():
     bg = pygame.transform.scale(bg, (SCREEN_WIDTH,SCREEN_HEIGHT))
 
     # Creación de los colliders del juego.
-    coll=Colliders(5)
+    coll=Colliders(10)
     main_clock = pygame.time.Clock()
 
+
+    # Mostramos el tiempo que se lleva jugando.
+    default_font = pygame.font.Font(None, FONT_SIZE)
 
     GAMEOVER = False
     exit= False
     
     # Reproducimos la música de fondo.
     pygame.mixer.music.play(2)
-    pygame.mixer.music.set_volume(0.1)
+    pygame.mixer.music.set_volume(SOUND_VOLUME)
 
     while not exit:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit = True
         if not GAMEOVER:
+            # Actualizamos el tiempo de juego mientras el jugador no haya perdido.
+            time = default_font.render("Time: "+str(int(pygame.time.get_ticks()/1000)), 0, (255,0,0))
             #Gestión del movimiento de 'player'.
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
@@ -174,6 +186,9 @@ def main():
         
         # Regeneramos los colliders.
         coll.re_add()
+
+        # Hacemos blit del texto que muestra el tiempo de juego.
+        screen.blit(time, (0,0))       
 
         pygame.display.update()
 
